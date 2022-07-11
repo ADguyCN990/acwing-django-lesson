4e8b0c8c3aa766beb73ce40457a60977f751fa15
ADguyCN990 <JHjinhui990@outlook.com>
2022-07-11 19:39:25 +0800

    update structure

---
+++ game/urls/index.py
@@ -0,0 +1,9 @@
+from game.views.index import index
+from django.urls import path, include
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("menu/", include("game.urls.menu.index")),
+    path("playground/", include("game.urls.playground.index")),
+    path("settings/", include("game.urls.settings.index")),
+]
